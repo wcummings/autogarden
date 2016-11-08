c5d9e388378a84d1797d3c53feb69f848ddbccc5
William Cummings <wcummings@Williams-MacBook-Pro.local>
2016-11-08 11:37:12 -0500

    Rearrange files

--- php/log.php
+++
@@ -1,6 +0,0 @@
-<?php
-$log = './log.txt';
-$data = json_decode(file_get_contents('php://input'), true);
-$data['ts'] = time()*1000;
-file_put_contents($log, json_encode($data) . "\n", FILE_APPEND | LOCK_EX);
-?>
